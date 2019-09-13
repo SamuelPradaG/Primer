ca2c0cd321af65891f8170b61dcb8c4f8121aa45
U-CINFO\bspradag <bspradag@PCSAL48573.cinfo.eafit.edu.co>
2019-09-13 18:55:07 -0500

    add class repo

---
+++ oop/hello-world.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  cout << "Hola Mundo ¯\\_(ツ)_/¯" << endl;
+	for(int i = 0; i <= 100; i ++){
+       cout << i << endl;
+	}
+	cout <<"Chau Mundo Cruel";
+}
